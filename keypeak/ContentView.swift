6b404f265451f3cffcf860379ce63988c895b881
Satomi Yoshio <satomi.yoshio@diamond-f.co.jp>
2025-12-01 15:16:53 +0900

    Initial Commit

---
+++ keypeak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  keypeak
+//
+//  Created by Yoshio Satomi  on 2025/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ keypeak/keypeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  keypeakApp.swift
+//  keypeak
+//
+//  Created by Yoshio Satomi  on 2025/12/01.
+//
+
+import SwiftUI
+
+@main
+struct keypeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
